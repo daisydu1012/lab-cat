b87f51651c8d6d47ae943a158d104134aa52135e
daisydu1012 <daisydu1012@gmail.com>
2026-02-01 14:06:54 -0800

    Make cat.py O(1) memory

--- cat.py
+++ cat.py
@@ -4,8 +4,11 @@ This program prints stdin to the screen.
 import sys
 
 def cat(file):
-    data = file.read()
-    sys.stdout.buffer.write(data)
+    while True:
+        chunk = file.read(1024 * 1024)  # 1MB
+        if not chunk:
+            break
+        sys.stdout.buffer.write(chunk)
 
 if __name__ == "__main__":
     if len(sys.argv) > 1:
